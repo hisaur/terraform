202b921a532394513b50a9fced73ebc7b90f676f
Aleksandr Toktosunov <aleksandr.toktosunov@greensky.com>
2021-07-28 22:44:32 +0600

    Moved code to folder structure

---
+++ nginx-ec2-autoscaling-group/nginx cluster.tf
@@ -0,0 +1,144 @@
+provider "aws" {
+    profile = "default"
+    region  = "us-east-1"
+}
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = "terraform-toktosunov-s3"
+    acl    = "private"
+    tags = {
+    "Creator" = "Terraform"
+  }
+}
+resource "aws_vpc" "new_vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+    "Creator" = "Terraform"
+  }
+}
+resource "aws_default_route_table" "terraform" {
+  default_route_table_id = aws_vpc.new_vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terraform.id
+  }
+}
+
+resource "aws_internet_gateway" "terraform" {
+  vpc_id = aws_vpc.new_vpc.id
+  tags = {
+    "Creator" = "Terraform"
+  }
+}
+resource "aws_subnet" "new_vpc_subnet-1" {
+  vpc_id            = aws_vpc.new_vpc.id
+  cidr_block        = "10.1.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    "Creator" = "Terraform"
+  }
+}
+resource "aws_subnet" "new_vpc_subnet-2" {
+  vpc_id            = aws_vpc.new_vpc.id
+  cidr_block        = "10.1.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    "Creator" = "Terraform"
+  }
+}
+resource "aws_security_group" "web" {
+    name        = "terraform_sg"
+    description = "simple descrition"
+    vpc_id      = aws_vpc.new_vpc.id
+    ingress     = [ {
+      cidr_blocks      = [ "0.0.0.0/0" ]
+      description      = "value"
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = [ ]
+      self             = false
+      to_port          = 443
+    },
+    {
+      cidr_blocks      = [ "0.0.0.0/0" ]
+      description      = "value"
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = [ ]
+      self             = false
+      to_port          = 80
+    },
+    {
+      cidr_blocks      = [ "0.0.0.0/0" ]
+      description      = "value"
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = [ ]
+      self             = false
+      to_port          = 22
+    }]
+    egress = [ {
+      cidr_blocks = [ "0.0.0.0/0" ]
+      description = "value"
+      from_port = 0
+      ipv6_cidr_blocks = [  ]
+      prefix_list_ids  = [  ]
+      protocol         = "-1"
+      security_groups  = [  ]
+      self             = false
+      to_port          = 0
+    } ]
+    tags = {
+    "Creator" = "Terraform"
+  }
+    
+}
+resource "aws_autoscaling_group" "terraform" {
+  name     = "nginx-autoscaling-group"
+  max_size = 3
+  min_size = 2
+  vpc_zone_identifier = [aws_subnet.new_vpc_subnet-1.id,aws_subnet.new_vpc_subnet-2.id]
+  launch_template {
+    id = aws_launch_template.terraform.id
+  }
+  
+  #target_group_arns = [ aws_lb_target_group.terraform_elb.arn]
+}
+resource "aws_launch_template" "terraform" {
+  name_prefix            = "terraform-nginx"
+  instance_type          = "t2.micro"
+  image_id               = "ami-0ad235070aed081b7"
+  key_name               = "default"
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [ aws_security_group.web.id ]
+  }
+}
+/*
+resource "aws_lb" "terraform_elb" {
+  internal           = false
+  subnets            = [ aws_subnet.new_vpc_subnet-1.id, aws_subnet.new_vpc_subnet-2.id ]
+  ip_address_type    = "ipv4"
+  load_balancer_type = "application"
+}
+resource "aws_lb_target_group" "terraform_elb" {
+  target_type = "instance"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.new_vpc.id
+}
+resource "aws_lb_listener" "terraform_elb" {
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.terraform_elb.arn
+  }
+  load_balancer_arn = aws_lb.terraform_elb.arn
+  protocol          = "HTTP"
+  port              = 80
+}
+*/
