ba8d6d78a67fe6d3b218552acdc45ba5f5f9c577
Aleksandr Toktosunov <aleksandr.toktosunov@greensky.com>
2021-07-28 16:51:10 +0600

    Changed Ec2 to Autoscaling group

--- s3.tf
+++ s3.tf
@@ -58,68 +58,38 @@ resource "aws_security_group" "web" {
     } ]
     
 }
-resource "aws_ebs_volume" "terraform-ssd" {
-  availability_zone = "us-east-1a"
-  size = 40
-  type = "gp2"
-  
-  tags = {
-    Creator = "Terraform"
-  }
-}
-resource "aws_autoscaling_group" "terraform-nginx" {
+resource "aws_autoscaling_group" "terraform" {
   name = "nginx-autoscaling-group"
   max_size = 3
   min_size = 2
-  vpc_zone_identifier = [aws_subnet.new_vpc_subnet-1,aws_subnet.new_vpc_subnet-2]
   
-}
+  vpc_zone_identifier = [aws_subnet.new_vpc_subnet-1.id,aws_subnet.new_vpc_subnet-2.id]
+  launch_template {
+    id = aws_launch_template.terraform.id
+  }
+  target_group_arns = [ aws_lb_target_group.terraform_elb.arn]
 
-resource "aws_launch_template" "terraform-nginx" {
-  name = "terraform-nginx-template"
+}
+resource "aws_launch_template" "terraform" {
+
+  name_prefix = "terraform-nginx"
   instance_type = "t2.micro"
   image_id = "ami-0ad235070aed081b7"
-  block_device_mappings {
-    device_name = "/dev/sdc1"
-    ebs {
-      volume_size = 20
-    }
-  }
-  
-}
-resource "aws_instance" "aws_linux" {
-  ami           = "ami-0dc2d3e4c0f9ebd18"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.web.id ]
-  
-  ebs_block_device {
-    device_name = "/dev/sdc"
-    volume_size = 40
-    volume_type = "gp2" 
-  }
-  subnet_id = aws_subnet.new_vpc_subnet-1.id
   key_name = "default"
-  tags = {
-    Creator = "Terraform"
-  }
+  vpc_security_group_ids = [ aws_security_group.web.id ]
 }
+
 resource "aws_lb" "terraform_elb" {
   internal = true
   subnets = [ aws_subnet.new_vpc_subnet-1.id, aws_subnet.new_vpc_subnet-2.id ]
   ip_address_type = "ipv4"
-  load_balancer_type = "network"
-}
-resource "aws_lb_target_group_attachment" "terraform_elb" {
-  target_group_arn = aws_lb_target_group.terraform_elb.arn
-  target_id = aws_instance.aws_linux.private_ip
-
+  load_balancer_type = "application"
 }
 resource "aws_lb_target_group" "terraform_elb" {
-  target_type = "ip"
+  target_type = "instance"
   port = 80
-  protocol = "TCP"
+  protocol = "HTTP"
   vpc_id = aws_vpc.new_vpc.id
-
 }
 resource "aws_lb_listener" "terraform_elb" {
   default_action {
@@ -127,6 +97,6 @@ resource "aws_lb_listener" "terraform_elb" {
     target_group_arn = aws_lb_target_group.terraform_elb.arn
   }
   load_balancer_arn = aws_lb.terraform_elb.arn
-  protocol = "TCP"
+  protocol = "HTTP"
   port = 80
 }
