c3981082e9ed295b0c24e8910c09e0d2e16b9352
Aleksandr Toktosunov <aleksandr.toktosunov@greensky.com>
2021-08-12 19:47:05 +0600

    Added docker repo. Added lock file to the repos

---
+++ docker-nginx/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {
+  host = "ssh://alex@127.0.0.1:22"
+}
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
